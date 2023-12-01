d24e588cf4cd20540f542589ea29fe9b280d31b5
gouransh-s <gouransh@iterasolutions.co.in>
2023-12-01 20:05:08 +0530

    Initial Bloom With API Implementation

---
+++ README.md
@@ -0,0 +1,14 @@
+## Install Dependencies
+
+```bash
+pip install -r requirements.txt
+```
+
+## Run Locally
+
+```bash
+python3 main.py
+```
+
+Execution 
+Above command will start a local server on port 5000 .
---
+++ main.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request
+
+from transformers import BloomForCausalLM
+from transformers import BloomTokenizerFast
+
+
+tokenizer = BloomTokenizerFast.from_pretrained("bigscience/bloom-1b7")
+model = BloomForCausalLM.from_pretrained("bigscience/bloom-1b7")
+
+
+# prompt = """Tell me something about"""
+result_length = 150
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def initial():
+  return render_template('index.html')
+
+
+@app.route('/submit-prompt', methods=['POST'])
+def generate():
+  #get the prompt input
+  prompt = request.form['prompt-input']
+  print(f"Generating response from bloom for: {prompt}")
+
+  inputs = tokenizer(prompt, return_tensors="pt")
+
+  #generate text
+  generated_text = ""
+  
+  # Sampling Top-k + Top-p
+  bloom_1b7 = tokenizer.decode(model.generate(inputs["input_ids"],
+                                                max_length = result_length,
+                                                do_sample= True,
+                                                top_k = 50,
+                                                top_p = 0.9
+                                                )[0])
+  chars_per_line = 75
+  print('Q: ', prompt)
+  print('')
+  print('A: ')
+
+  for i in range(0, len(bloom_1b7), chars_per_line):
+    print(bloom_1b7[i:i+chars_per_line])
+    generated_text = f"{generated_text}{bloom_1b7[i:i+chars_per_line]}"
+    
+  return render_template('index.html', generated_text=generated_text, prompt=prompt)
+
+if __name__ == '__main__':
+    app.run()
